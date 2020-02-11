fdfc700db1eea3f5d492f8a3af7e41b69ee40dd5
Student <example@example.com>
2020-02-11 11:17:33 -0800

    add content, restructure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import TicketList from "./components/TicketList"
 
-function App() {
+function App(props){
+  var greenApp = {
+    backgroundColor: '#003300',
+    color: '#ffff00'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={greenApp}>
+      <Header/>
+      <TicketList/>
     </div>
   );
 }
